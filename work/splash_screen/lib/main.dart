f8bd3611b70de1fe5864b837508607458e638760
gyeol <gksruf2233@gmail.com>
2025-11-24 16:49:04 +0900

    feat: Show Context Action 유틸 사용하여 위젯 제거 및 감싸기

--- work/splash_screen/lib/main.dart
+++ work/splash_screen/lib/main.dart
@@ -19,24 +19,25 @@ class HomeScreen extends StatelessWidget {
         /// 335CB0
         backgroundColor: Color(0xFF335CB0),
         body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 32.0,
-          ),
-          child: Column(
-            /// 중앙 정렬
-            mainAxisAlignment: MainAxisAlignment.center,
-            /// children 파라미터는 여러 개의 위젯을 배열 형태로 전달
-            children: [
-              Image.asset(
-                'asset/img/logo.png',
+          padding: EdgeInsets.symmetric(horizontal: 32.0),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              /// 중앙 정렬
+              mainAxisAlignment: MainAxisAlignment.center,
+              /// children 파라미터는 여러 개의 위젯을 배열 형태로 전달
+              children: [
+                Image.asset(
+                  'asset/img/logo.png',
+                ),
+                SizedBox(height: 28.0),
+                CircularProgressIndicator(
+                  color: Colors.white,
               ),
-              SizedBox(height: 28.0),
-              CircularProgressIndicator(
-                color: Colors.white,
-            ),
-          ],
+            ],
+                    ),
+          ),
         ),
-      ),
     );
   }
 }
