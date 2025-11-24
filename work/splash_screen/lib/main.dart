d25a40d084c3f1052d8804b94cc36e8e539a61a1
gyeol <gksruf2233@gmail.com>
2025-11-24 15:26:51 +0900

    feat: CircularProgresIndicator 위젯을 사용하여 로딩상태 표현하기

--- work/splash_screen/lib/main.dart
+++ work/splash_screen/lib/main.dart
@@ -8,10 +8,15 @@ void main() {
         /// 335CB0
         backgroundColor: Color(0xFF335CB0),
         body: Column(
+          /// 중앙 정렬
           mainAxisAlignment: MainAxisAlignment.center,
+          /// children 파라미터는 여러 개의 위젯을 배열 형태로 전달
           children: [
             Image.asset(
               'asset/img/logo.png',
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
             ),
           ],
         ),
