da4fd1d17d8422ecf6e75ba72165dd27b88e8dbb
gyeol <gksruf2233@gmail.com>
2025-11-25 15:10:26 +0900

    feat: Column CrossAxisAlignment로 Column의 주축인 수직방향으로 자식 위젯들을 배치하는 방법

--- work/row_and_column/lib/screen/home_screen.dart
+++ work/row_and_column/lib/screen/home_screen.dart
@@ -10,9 +10,12 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Container(
           color: Colors.black,
-          child: Row(
+          width: double.infinity, /// 최대한의 크기 지정
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
+            //crossAxisAlignment: CrossAxisAlignment.center, // 기본값은 center
+            crossAxisAlignment: CrossAxisAlignment.stretch, // 컬럼의 크기를 최대한으로 늘림
             children: colors.map(
               (e) => Container(
                 height: 50.0,
