4c10534c7b8997ef2b5accd2d2c5bfdd8d0f1a2c
gyeol <gksruf2233@gmail.com>
2025-11-26 17:23:28 +0900

    feat: URI 생성하여 웹 사이트 웹뷰에 띄우기

--- work/web_view/lib/screen/home_screen.dart
+++ work/web_view/lib/screen/home_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  /// final result = WebViewController().loadRequest();
+  /// final controller = WebViewController();
+  /// final result = controller.loadRequest();
+  ///
+  /// final test2 = WebViewController()..loadRequest();
+  ///
+  /// final controller2 = WebViewController();
+  /// controller2.loadRequest();
+
+  WebViewController controller = WebViewController()
+  ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
