2ca68012f1c7c3047d0ee58b650298a959524425
gyeol <gksruf2233@gmail.com>
2025-11-26 21:11:44 +0900

    feat: StatelessWidget 라이프 사이클 과정(생성자실행 > 빌드실행)

---
+++ work/statelesswidget_lifecycle/lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:statelesswidget_lifecycle/screen/home_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CodeFactoryWidget(
+
+      ),
+    );
+  }
+}
+
+class CodeFactoryWidget extends StatelessWidget {
+  CodeFactoryWidget({super.key}){
+    print('-- 생성자 ---');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('-- 빌드 ---');
+    return SafeArea(
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: Colors.red,
+      ),
+    );
+  }
+}
