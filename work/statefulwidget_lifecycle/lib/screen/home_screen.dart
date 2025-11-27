59effa0985d52e4eabe2e6e9f0bf9cd169c005f7
gyeol <gksruf2233@gmail.com>
2025-11-27 17:28:51 +0900

    feat: statefulWidget 라이프 사이클 간 각 메서드별 실행 순서 확인

--- work/statefulwidget_lifecycle/lib/screen/home_screen.dart
+++ work/statefulwidget_lifecycle/lib/screen/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool show = true;
+  bool show = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,15 +35,59 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class CodeFactoryWidget extends StatelessWidget {
-  const CodeFactoryWidget({super.key});
+/// StatefulWidget 라이프사이클 예제 위젯
+class CodeFactoryWidget extends StatefulWidget {
+  /// 1) Constructor: StatefulWidget 생성 시 호출 (State 객체는 아직 생성되지 않음)
+  CodeFactoryWidget({super.key}) {
+    print('1) Stateful Widget Constructor');
+  }
 
+  /// 2) createState: 위젯 트리에 추가될 때 State 객체 생성
+  @override
+  State<CodeFactoryWidget> createState() {
+    print('2) Stateful Widget createState');
+
+    return _CodeFactoryWidgetState();
+  }
+}
+
+class _CodeFactoryWidgetState extends State<CodeFactoryWidget> {
+  /// 3) initState: State 생성 후 한 번만 호출, 초기화 작업 수행
+  @override
+  void initState() {
+    print('3) Stateful Widget initState');
+    super.initState(); // 부모의 initState를 호출
+  }
+
+  /// 4) didChangeDependencies: initState 직후 및 InheritedWidget 의존성 변경 시 호출
+  @override
+  void didChangeDependencies() {
+    print('4) Stateful Widget didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  /// 5) build: 위젯 빌드 (setState 호출 시마다 재호출)
   @override
   Widget build(BuildContext context) {
+    print('5) Stateful Widget build');
     return Container(
         color: Colors.red,
         width: 50.0,
         height: 50.0,
     );
+  }
+
+  /// 6) deactivate: 위젯 트리에서 제거되기 직전 호출 (임시 정리 작업)
+  @override
+  void deactivate() {
+    print('6) Stateful Widget deactivate');
+    super.deactivate();
+  }
+
+  /// 7) dispose: State 완전 제거 시 한 번만 호출, 리소스 정리
+  @override
+  void dispose() {
+    print('7) Stateful Widget dispose');
+    super.dispose();
   }
 }
