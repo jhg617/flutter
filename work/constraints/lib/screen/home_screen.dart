24fea1d67462ef0b780d3f487e111e585507ff1c
gyeol <gksruf2233@gmail.com>
2025-12-02 16:36:57 +0900

    feat: Constraints 예제

---
+++ work/constraints/lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.red,
+            child: Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
