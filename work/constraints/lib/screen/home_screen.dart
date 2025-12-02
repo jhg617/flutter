04deb91a51bc93481e8c6fb7c5866544423491da
gyeol <gksruf2233@gmail.com>
2025-12-02 16:50:44 +0900

    chore: 주석추가

--- work/constraints/lib/screen/home_screen.dart
+++ work/constraints/lib/screen/home_screen.dart
@@ -9,12 +9,15 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Container(
+            // 크기 제약: 200x200으로 고정
             height: 200,
             width: 200,
             color: Colors.red,
             child: Row(
+              // 부모 Container의 제약(200x200) 내에서만 배치 가능
               children: [
                 Container(
+                  // 크기 제약: 50x50으로 고정
                   height: 50,
                   width: 50,
                   color: Colors.blue,
