8a91df016a1276f7e0f137506a39fbb361db31a9
gyeol <gksruf2233@gmail.com>
2025-12-04 16:49:18 +0900

    feat: Setting 화면에 렌더링하기

--- work/random_number_generator/lib/screen/setting_screen.dart
+++ work/random_number_generator/lib/screen/setting_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:random_number_generator/constant/color.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  double maxNumber = 1000;
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,73 @@ class SettingScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: redColor,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: (){
-                  Navigator.of(context).pop(); //뒤로가기
-                },
-                child: Text('저장'),
+              _Number(
+                maxNumber: maxNumber,
               ),
+              _Slider(),
+              _Button(),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      onPressed: (){
+        Navigator.of(context).pop(); //뒤로가기
+      },
+      child: Text('저장'),
+    );
+  }
+}
+
+class _Number extends StatelessWidget {
+  final double maxNumber;
+
+  const _Number({
+    required this.maxNumber,
+    super.key
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Row(
+          children: maxNumber
+              .toInt() // double을 int로 변환
+              .toString() // int를 String으로 변환
+              .split('')
+              .map(
+                (number) => Image.asset(
+                  'asset/img/$number.png',
+                  width: 50.0,
+                  height: 70.0,
+                ),
+              )
+              .toList(), // Iterable을 List로 변환 (Row의 children은 List<Widget> 필요)
+        ),
+      ),
+    );
+  }
+}
+
+class _Slider extends StatelessWidget {
+  const _Slider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
