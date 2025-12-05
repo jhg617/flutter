30632330fdf1208da58dddcc0c3925965d4281b5
gyeol <gksruf2233@gmail.com>
2025-12-05 18:11:34 +0900

    feat: 공통 레이아웃 컴포넌트 제작 및 페이지 전환

--- work/navigation/lib/screen/home_screen.dart
+++ work/navigation/lib/screen/home_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/default_layout.dart';
+import 'package:navigation/screen/route_one_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        
-      ),
+    return DefaultLayout(
+      title: 'HomeScreen',
+      children: [
+        OutlinedButton(
+              onPressed: (){
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return RouteOneScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text('Push'),
+        ),
+      ],
     );
   }
 }
