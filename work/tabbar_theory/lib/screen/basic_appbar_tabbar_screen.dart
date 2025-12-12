530934f7783ffe1d31c3eb873f26a271b7c1f8a0
gyeol <gksruf2233@gmail.com>
2025-12-12 12:44:29 +0900

    feat: TabBar 만들기

--- work/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ work/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -1,13 +1,34 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:tabbar_theory/const/tabs.dart';
 
 class BasicAppbarTabbarScreen extends StatelessWidget {
   const BasicAppbarTabbarScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      body: Column(
-        children: [],
+    return DefaultTabController(
+      length: TABS.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BasicAppBarScreen'),
+          bottom: TabBar(
+            tabs: TABS
+                .map(
+                  (e) => Tab(
+                    icon: Icon(
+                      e.icon,
+                    ),
+                    child: Text(
+                      e.label,
+                    ),
+                  ),
+                )
+                .toList( ),
+          ),
+        ),
+        body: Column(children: []),
       ),
     );
   }
