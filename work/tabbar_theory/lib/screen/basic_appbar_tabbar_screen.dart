a3c16c8dbde40fb0956946ef5dbfea21ed170a48
gyeol <gksruf2233@gmail.com>
2025-12-12 15:14:56 +0900

    style: TabBar 스타일링하기

--- work/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ work/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -13,22 +13,57 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text('BasicAppBarScreen'),
-          bottom: TabBar( // AppBar 하단에 탭 버튼들 배치
-            tabs: TABS
-                .map(
-                  (e) => Tab(
-                    icon: Icon(
-                      e.icon,
-                    ),
-                    child: Text(
-                      e.label,
-                    ),
+          centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(80),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TabBar(
+                  // AppBar 하단에 탭 버튼들 배치
+                  // 인디케이터 색상 변경
+                  indicatorColor: Colors.red,
+                  indicatorWeight: 4.0,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  isScrollable: true,
+                  labelColor: Colors.yellow,
+                  unselectedLabelColor: Colors.grey,
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.w700,
                   ),
-                )
-                .toList(), // TabBar의 tabs는 List<Tab> 타입 필요
+                  unselectedLabelStyle: TextStyle(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  tabs: TABS
+                      .map(
+                        (e) => Tab(
+                          icon: Icon(
+                            e.icon,
+                          ),
+                          child: Text(
+                            e.label,
+                          ),
+                        ),
+                      )
+                      .toList(), // TabBar의 tabs는 List<Tab> 타입 필요
+                ),
+              ],
+            ),
           ),
         ),
-        body: Column(children: []),
+        body: TabBarView(
+          // 좌우로 넘기는것 제한하기
+          physics: NeverScrollableScrollPhysics(),
+          children: TABS
+            .map(
+              (e) => Center(
+                child: Icon(
+                  e.icon,
+                ),
+              ),
+          )
+          .toList(),
+        ),
       ),
     );
   }
